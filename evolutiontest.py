30f797633c9fa466fb93858c71d6598b32798db3
ssocolow <47473649+ssocolow@users.noreply.github.com>
2019-11-11 14:40:22 -0500

    Update evolutiontest.py

--- evolutiontest.py
+++ evolutiontest.py
@@ -6,28 +6,64 @@ import random
 POPSIZE = 100
 nets = []
 num_of_gens = 0
-outputs = []
 
-#have data to determine what you want to do
+#have data to evolve the neural nets on
 house_inputs = [[0.2, 0.2, 0.175],[0.2, 0.2, 0.096],[0.3, 0.2, 0.18], [0.3, 0.3, 0.2238]]
 house_targets = [[0.16],[0.053],[0.129],[0.178]]
 
 def epoch():
+    #use the global variables
+    global nets
+    global num_of_gens
+
+    #array to store scores
+    scores = []
+
+    #sum of all the scores
+    total = 0
+
+    #an array to store an amount of index numbers for each neural net according to their probability
+    indicies_array = []
+
+    #array with probablilites of each network based on its score / total score
+    probabilities = []
+
     #nets is going to have arrays which have generations in the big array
     nets.append([])
-    outputs.append([])
 
     #fill the first array in nets with randomly initialized neural nets
     if num_of_gens == 0:
         for i in range(POPSIZE):
-            nets[num_of_gens].append(nn.NeuralNetwork([[3],[4],[1]], mutation_rate = 0.4))
-    else:
+            nets[0].append(nn.NeuralNetwork([[3],[4],[1]], mutation_rate = 0.25))
 
     for i in range(POPSIZE):
-        outputs[num_of_gens].append(nets[i].feedforward(house_inputs[0]))
+        #get the score of each network by finding absolute value of the difference between the network's output and the target
+        #this will give us a smaller number for a better score, so to have a higher number for a higher score, we can do 1 divided by the score so lower score becomes higher score
+        scores.append(1 / abs(nets[num_of_gens][i].feedforward(house_inputs[0])[0] - house_targets[0][0]))
+        total += scores[i]
 
     for i in range(POPSIZE):
-        nets
+        #get an array of probablilites for each neural net
+        probabilities.append(scores[i] / total)
+
+    for i in range(POPSIZE):
+        for j in range(round(probabilities[i] * 100)):
+            indicies_array.append(i)
+
+    #add another array to store the next generation
+    nets.append([])
+
+    for i in range(POPSIZE):
+        nets[num_of_gens + 1].append(nets[num_of_gens][random.choice(indicies_array)].copy().mutate())
+
+    num_of_gens += 1
+
+for i in range(50):
+    epoch()
+
+print(nets[num_of_gens][0].feedforward(house_inputs[0]))
+#progress notes
+#the copy function has a problem, and I can't get the get_data function to work
 
 #have an array with all the outputs
 #have an array with 1 / (target - output)^2 (squared to get rid of sign)
