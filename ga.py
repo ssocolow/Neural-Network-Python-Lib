d4bd2e18633de92c82343051d3fdc971ebc986b9
Simon Socolow <simonsocolow78@gmail.com>
2020-03-11 22:01:13 +0000

    updates

--- ga.py
+++ ga.py
@@ -34,6 +34,34 @@ def pickOne(list_):
     #return that index
     return index
 
+#takes an array with probabilities (numbers between 0 and 1 that sum to 1) and picks one based on its probability in an ingenious way (found on coding train's github) (https://github.com/CodingTrain/website/blob/master/CodingChallenges/CC_100.1_NeuroEvolution_FlappyBird/P5/ga.js)
+#returns the index of the chosen one in the list
+def pickOneProbs(list_):
+    #start the index at 0
+    index = 0
+    #generate a random number between 0 and 1
+    r = random.random()
+    #while that number is greater than 0 (still positive), subtract from it an agent's probablility and increment the index
+    #then it returns 1 more than the index of the agent that made r go negative
+    while r > 0:
+        r = r - list_[index]
+        index = index + 1
+    #subtract 1 to get back to the right index
+    index = index - 1
+    #return that index
+    return index
+
+#takes an array of numbers as input
+#outputs an array of probabilities that correspond with those numbers
+def makeProbs(array):
+    sum_ = 0
+    probs = []
+    for n in array:
+        sum_ += n
+    for n in array:
+        probs.append(n / sum_)
+    return probs
+
 #takes an array of objects with a score property and assigns a probability value to their probability property
 def calculateFitness(list_):
     sum_ = 0
