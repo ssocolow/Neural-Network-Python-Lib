e942e53aacc9bd110c07fd2f4e04d6b0acbc4043
Simon Socolow <simonsocolow78@gmail.com>
2020-04-05 15:33:59 +0000

    updating and now uses relu

--- evolutiontest.py
+++ evolutiontest.py
@@ -1,74 +1,76 @@
 #import libraries
 import nn
+import ga
 import random
 
 #have global storing variables
+#POPSIZE is how many networks in each generation
 POPSIZE = 100
-nets = []
+#make a variable to hold the number of generations
 num_of_gens = 0
+#make an array to hold all of the nets
+nets = []
 
-#have data to evolve the neural nets on
+#have data to evolve the neural nets on to make a house price predictor based on the number of bathrooms, bedrooms, and square footage
+#taking the first input [0.2, 0.2, 0.175] as an example, that house has 2 bathrooms, 2 bedrooms, and 1,750 square feet
+#this house has a price of 160,000 dollars
 house_inputs = [[0.2, 0.2, 0.175],[0.2, 0.2, 0.096],[0.3, 0.2, 0.18], [0.3, 0.3, 0.2238]]
 house_targets = [[0.16],[0.053],[0.129],[0.178]]
 
-def epoch():
+
+#make a fitness function that will return a score for the network based on how well it did
+#higher is better
+def evaluateNetwork(net):
+    __sum = 0
+    for i in range(len(house_inputs)):
+        __sum += abs(1/(net.feedforward(house_inputs[i])[0] - house_targets[i][0]))
+    return __sum
+
+
+#make an evolve function
+def evolve():
     #use the global variables
-    global nets
     global num_of_gens
+    global nets
+    global POPSIZE
 
-    #array to store scores
-    scores = []
+    print(num_of_gens)
 
-    #sum of all the scores
-    total = 0
-
-    #an array to store an amount of index numbers for each neural net according to their probability
-    indicies_array = []
-
-    #array with probablilites of each network based on its score / total score
-    probabilities = []
-
-    #nets is going to have arrays which have generations in the big array
-    nets.append([])
-
-    #fill the first array in nets with randomly initialized neural nets
+    #if it is the first generation, create the first nets
     if num_of_gens == 0:
+        nets.append([])
         for i in range(POPSIZE):
-            nets[0].append(nn.NeuralNetwork([[3],[4],[1]], mutation_rate = 0.1))
+            nets[0].append(nn.NeuralNetwork([[3],[32],[16],[1]]))
 
-    for i in range(POPSIZE):
-        #get the score of each network by finding absolute value of the difference between the network's output and the target
-        #this will give us a smaller number for a better score, so to have a higher number for a higher score, we can do 1 divided by the score so lower score becomes higher score
-        scores.append(1 / abs(nets[num_of_gens][i].feedforward(house_inputs[0])[0] - house_targets[0][0]))
-        total += scores[i]
+    #loop through all of the nets of the current generation
+    for net in nets[num_of_gens]:
+        net.score = evaluateNetwork(net)
+        #print(str(nets[num_of_gens].index(net)) + ' / ' + str(POPSIZE))
 
-    for i in range(POPSIZE):
-        #get an array of probablilites for each neural net
-        probabilities.append(scores[i] / total)
-
-    for i in range(POPSIZE):
-        for j in range(round(probabilities[i] * 100)):
-            indicies_array.append(i)
-
-    #add another array to store the next generation
+    #get ready to spawn the next generation
     nets.append([])
 
-    for i in range(POPSIZE):
-        nets[num_of_gens + 1].append(nets[num_of_gens][random.choice(indicies_array)].copy().mutate())
+    #make the probabilities for the networks to be picked
+    nets[num_of_gens + 1].extend(ga.nextGeneration(nets[num_of_gens]))
 
+    #increment the number of generations
     num_of_gens += 1
 
-for i in range(100):
-    epoch()
 
 
+
+#run evolve certain number of times
+def runEvolve(n):
+    for i in range(n):
+        evolve()
+
+#run 25 epochs
+runEvolve(25)
+
+#weird issue that arises is the first networks have a prediction of 0 for the house price, could come from using too much memory?
+
+#print what the first network thought that a house which has 2 bathrooms, 2 bedrooms, and 1,750 square feet should cost
 print(nets[0][0].feedforward(house_inputs[0]))
+#then print what one of the networks in the last generation thinks it costs
+#the number should be closer to 0.16 than the first
 print(nets[num_of_gens][0].feedforward(house_inputs[0]))
-# progress notes
-# the copy function has a problem, and I can't get the get_data function to work
-
-# have an array with all the outputs
-# have an array with 1 / (target - output)^2 (squared to get rid of sign)
-# have the networks attempt all data
-# with the score array, add them all up and then divide each one by the total for the probablility
-# have an array with numbers equal to the index of the array which has the nets, number of numbers corresponds to the probablility
